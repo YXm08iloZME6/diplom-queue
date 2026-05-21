f39c5ae880f0f56a605638c60dcdc072027e0e5f
Aydar <gasbi4ok@gmail.com>
2026-05-21 18:19:11 +0400

    restore signalR clean state

--- Domain/Entities/Service.cs
+++ Domain/Entities/Service.cs
@@ -7,9 +7,10 @@ public class Service
     public string? Description { get; set; }
     public string? Letter { get; set; }
     public string? IconName { get; set; }
+
     public bool IsActive { get; set; } = true;
     public bool IsNeedFacets { get; set; } = true;
-    
+
     public Guid? ParentId { get; set; }
     public Service? Parent { get; set; }
     public ICollection<Service> Children { get; set; } = new List<Service>();
